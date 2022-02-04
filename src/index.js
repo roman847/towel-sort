6ec8882732c91b68646a5b49070433ad7ddabae3
Roman <romabogdan147@gmail.com>
2022-02-04 19:43:33 +0300

    feat: add task (Fri Feb  4 19:43:33 RTZ 2022)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+// module.exports = function towelSort (matrix) {
+  function res (matrix) {
+   
+
+    
+    
+    module.exports = function towelSort (matrix) {
+      return matrix.reduce((acc,el,i) =>{
+        console.log(matrix)
+        if(i %2 == 0) {
+          console.log(el)
+          el.forEach(element => acc.push(element))
+        }else {
+          el.reverse().forEach(element => acc.push(element))
+          
+        }
+        return acc
+      },[])
+    }
